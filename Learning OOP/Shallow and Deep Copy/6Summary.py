ca989bd5ad52fa839e29fb2caeab32da96010949
DjangoBlockchained <74975524+Lewis-T-543@users.noreply.github.com>
2021-04-27 16:11:02 +0100

    Delete Learning OOP directory

--- Learning OOP/Shallow and Deep Copy/6Summary.py
+++
@@ -1,9 +0,0 @@
-                                #### SUMMARY ####
-
-# The deepcopy() method creates and persists new instances of source objects, 
-# whereas any shallow copy operation only stores references to the original memory address
-
-# A deep copy operation takes significantly more time than any shallow copy operation
-
-# The deepcopy() method copies the whole object, including all nested objects; it’s an example of practical recursion taking place;
-# deep copy might cause problems when there are cyclic references in the structure to be copied.
